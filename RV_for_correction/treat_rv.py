4cb8d40d80f73ab43f55e6b14bd2df1defea620e
pedrobranco1999 <79382849+pedrobranco1999@users.noreply.github.com>
2023-05-23 18:58:15 +0100

    Update treat_rv.py
    This code needs to be verified.

--- RV_for_correction/treat_rv.py
+++ RV_for_correction/treat_rv.py
@@ -3,14 +3,22 @@ import pandas as pd
 from astropy.io import fits
 import os
 
+PATH_TO_SAVE='/Spectra/'
 
-def treat_rv(name_file,rv,folder):
+"""
+You can run this program for a single file and can be adapted to run for a list of spectrums.
+You just need to create a csv file with the names of the spectrums and the rv values for each one.
+Make sure that you save the csv file in this folder.
+"""
+
+
+def treat_rv(name_file,rv):
     
     """
     This function does the correction of the spectrum in radial velocity.
     Parameters:
     -name_file: The name of the file to correct the spectrum.
-    -rv: Value of the radial velocity of the specrtum    
+    -rv: Value of the radial velocity of the specrtum.
     """
  
     c=299792.458 ### light velocity
@@ -38,45 +46,4 @@ def treat_rv(name_file,rv,folder):
     Header["RV"] = rv
     New_name=nome0 + "_rv.fits"
 
-    fits.writeto(folder + New_name,Flux_int,Header,overwrite=True)
-
-#Stars=pd.read_csv("stars_information_others_spec.csv")
-
-#List_fit_unr=list(Stars["fits_name_unresolved"])
-#List_rv=list(Stars["rv"])
-
-if False:
-    for i in range(len(List_fit_unr)):
-        treat_rv(List_fit_unr[i],List_rv[i],"/home/pedro/OneDrive/Documentos/codes/Espetros/Other_spec_treat/")
-
-def treat_rv_one_file(name_file,rv,folder):
-    c=299792.458 ### light velocity
-    
-    File_op=fits.open(name_file)
-    Data=File_op[0].data
-    Header=File_op[0].header
-    nome0=os.path.basename(name_file.split(".fits")[0])
-
-    File_op.close()
-    wave_start = Header['CRVAL1']
-    wave_delta = Header['CDELT1']
-    wave = np.arange(Data.size) * wave_delta + wave_start
-
-    wave_corr=wave/(1+rv/c)
-
-    wave_int=np.arange(wave_corr[0],wave_corr[-1],wave_delta)
-
-    Flux_int=np.interp(wave_int,wave_corr,Data)
-
-    Header["CDELT1"]=wave_delta
-    Header["CRVAL1"]=wave_int[0]
-    Header["COMMENT"]= "Corrigido na velocidade radial."
-    Header["RV"] = rv
-    New_name=nome0 + "_rv.fits"
-    print(2)
-    #fits.writeto(folder + New_name,Flux_int,Header,overwrite=True)
-    fits.writeto("KPS-1_SOPHIE_HE_2020_s_rv.fits",Flux_int,Header,overwrite=True)
-
-
-#name1="/home/pedro/OneDrive/Documentos/codes/Espetros/Other_spec_treat/Kepler-39_SOPHIE_HE_2020.fits"
-treat_rv_one_file('KPS-1_SOPHIE_HE_2020_s.fits',2.3,"/home/pedro/OneDrive/Documentos/codes/Espetros/")
+    fits.writeto(PATH_TO_SAVE + New_name,Flux_int,Header,overwrite=True)
